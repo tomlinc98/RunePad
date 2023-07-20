dcb692a82f186f8bfc6514368a9f41562a7bed91
Chris <tomlinc98@gmail.com>
2023-07-20 14:10:40 +0100

    added tabs feature

--- RunePad.lua
+++ RunePad.lua
@@ -8,25 +8,11 @@ function RunePad:Initialize()
 
     -- Connect UI elements
     self.window = RunePadWindow
-    self.tabs = {
-        ["Tab1"] = {
-            button = RunePadTab1,
-            box = RunePadWindowTab1Box
-        },
-        ["Tab2"] = {
-            button = RunePadTab2,
-            box = RunePadWindowTab2Box
-        }
-    }
+    self.tabs = {}
 
-    -- Define the tabs' behavior
-    for tabName, tab in pairs(self.tabs) do
-        tab.button:SetHandler("OnMouseUp", function() self:TabClicked(tabName) end)
-        tab.box:SetHidden(true) -- Hide all boxes by default
-    end
-
-    -- Show the first tab by default
-    self:TabClicked("Tab1")
+    -- Connect the "Add Tab" button and set its handler
+    self.addTabButton = RunePadAddTabButton
+    self.addTabButton:SetHandler("OnMouseUp", function() self:AddTab() end)
 
     -- Handle moving of the RunePad window
     self.window:SetHandler("OnMouseUp", function() self.window:SetMovable(false) end)
@@ -37,6 +23,7 @@ function RunePad:Initialize()
     
     d("RunePad initialized.")
 end
+
 
 function RunePad:TabClicked(tabName)
     if self.activeTab then
@@ -57,6 +44,38 @@ function RunePad.OnAddOnLoaded(event, addonName)
     if addonName == RunePad.name then
         RunePad:Initialize()
     end
+end
+
+function RunePad:AddTab()
+    -- Generate a unique tab name
+    local tabName = "Tab" .. tostring(#self.tabs + 1)
+
+    -- Create a new button for the tab
+    local newTabButton = WINDOW_MANAGER:CreateControl("RunePadTab" .. tabName, RunePadWindow, CT_BUTTON)
+    newTabButton:SetDimensions(100, 30)
+    
+    -- Position the new tab next to the last tab
+    local offsetX = 10 + #self.tabs * 110 -- 110 = width of a tab + 10 for spacing
+    newTabButton:SetAnchor(TOPLEFT, RunePadWindow, TOPLEFT, offsetX, 10)
+    
+    -- Create the text box for the new tab
+    local newTextBox = WINDOW_MANAGER:CreateControlFromVirtual("RunePadWindowTab" .. tabName .. "Box", RunePadWindow, "ZO_DefaultEditForBackdrop")
+    newTextBox:SetDimensions(480, 450)
+    newTextBox:SetAnchor(TOPLEFT, newTabButton, BOTTOMLEFT, 0, 10)
+    newTextBox:SetHidden(true)
+
+    -- Add the new tab to the tabs table
+    self.tabs[tabName] = {
+        button = newTabButton,
+        box = newTextBox
+    }
+
+    -- Set up the OnMouseUp handler for the new tab
+    newTabButton:SetHandler("OnMouseUp", function() self:TabClicked(tabName) end)
+
+    -- Update the position of the "Add Tab" button
+    offsetX = offsetX + 110
+    self.addTabButton:SetAnchor(TOPLEFT, RunePadWindow, TOPLEFT, offsetX, 10)
 end
 
 -- Event handlers
